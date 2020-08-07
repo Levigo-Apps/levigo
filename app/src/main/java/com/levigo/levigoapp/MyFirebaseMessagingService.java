298e867664b3be61154bed46cfa87264f86d6cff
Riya Saha <rs3796@barnard.edu>
2020-08-07 13:38:18 -0400

    Update Services to handle notifications on the frontend

--- app/src/main/java/com/levigo/levigoapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/levigo/levigoapp/MyFirebaseMessagingService.java
@@ -3,14 +3,50 @@ package com.levigo.levigoapp;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
-public class MyFirebaseMessagingService extends Service {
-    public MyFirebaseMessagingService() {
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
+
+        if(remoteMessage == null){
+            return;
+        }
+
+        String messageTitle = remoteMessage.getData().get("header");
+        String messageBody = remoteMessage.getData().get("body");
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(messageTitle)
+                .setContentText(messageBody)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Log.d("Message", "FCM Received");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, builder.build());
+
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onNewToken(String token) {
+        Log.d("Token", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
     }
 }
