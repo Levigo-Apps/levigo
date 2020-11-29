f030eeb9d1bc382329ae5ebbee2080279040fa38
tykosc <talyakosch@gmail.com>
2020-11-29 10:00:01 -0500

    autopopulate from GUDID

--- app/src/main/java/org/getcarebase/carebase/api/AccessGUDIDAPI.java
+++ app/src/main/java/org/getcarebase/carebase/api/AccessGUDIDAPI.java
@@ -1,5 +1,6 @@
 package org.getcarebase.carebase.api;
 
+import org.getcarebase.carebase.models.DeviceModel;
 import org.getcarebase.carebase.models.ParseUDIResponse;
 
 import retrofit2.Call;
@@ -9,4 +10,8 @@ import retrofit2.http.Query;
 public interface AccessGUDIDAPI {
     @GET("parse_udi.json")
     Call<ParseUDIResponse> getParseUdiResponse(@Query("udi") String udi);
+
+    @GET("devices/lookup.json")
+    Call<DeviceModel> getDeviceModel(@Query("udi") String udi);
+
 }
