dafa49f61f1b62466ae7a924d45f9c2ea9f2c735
Saketh Dargula <sdargula@gmail.com>
2021-01-15 11:51:03 -0500

    refactored to login screens to use fragments so that androidx navigation can be used

--- app/src/main/java/org/getcarebase/carebase/activities/Login/LoginActivity.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Login/LoginActivity.java
@@ -18,30 +18,16 @@ package org.getcarebase.carebase.activities.Login;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseTooManyRequestsException;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 import org.getcarebase.carebase.activities.Main.MainActivity;
 import org.getcarebase.carebase.R;
@@ -55,111 +41,36 @@ import org.getcarebase.carebase.viewmodels.AuthViewModel;
  */
 
 public class LoginActivity extends AppCompatActivity {
-
     private static final String TAG = LoginActivity.class.getSimpleName();
-    private static final int RC_SIGN_IN = 1;
-
-    private EditText mEmail, mPassword;
-    private TextInputLayout emailTextInputLayout,passwordTextInputLayout;
-    private Button loginButton;
-
-    private AuthViewModel authViewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
-        setContentView(R.layout.activity_login);
-        authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+        setContentView(R.layout.login_host);
+        AuthViewModel authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
 
-        authViewModel.getUserLiveData().observe(this, new Observer<Resource<User>>() {
-            @Override
-            public void onChanged(Resource<User> userResource) {
-                if (userResource.getRequest().getStatus() == Request.Status.SUCCESS) {
-                    // sign in user
-                    signUserIn(userResource.getData());
-                }
-                else if (userResource.getRequest().getStatus() == Request.Status.ERROR && userResource.getRequest().getResourceString() != null) {
-                    if (userResource.getRequest().getResourceString() == R.string.error_invalid_email_format) {
-                        emailTextInputLayout.setError(getString(userResource.getRequest().getResourceString()));
-                    }
-                    else if (userResource.getRequest().getResourceString() == R.string.error_invalid_email_or_password) {
-                        emailTextInputLayout.setError(getString(userResource.getRequest().getResourceString()));
-                        passwordTextInputLayout.setError(getString(userResource.getRequest().getResourceString()));
-                    }
-                    else if (userResource.getRequest().getResourceString() == R.string.error_too_many_attempts ||
-                            userResource.getRequest().getResourceString() == R.string.error_something_wrong) {
-                        Snackbar.make(findViewById(R.id.login_constraint_layout), userResource.getRequest().getResourceString(), Snackbar.LENGTH_LONG).show();
-                    }
-                }
+        authViewModel.getUserLiveData().observe(this, userResource -> {
+            if (userResource.getRequest().getStatus() == Request.Status.SUCCESS) {
+                signUserIn();
             }
         });
 
+        // go to login screen
+        if (savedInstanceState == null) {
+            Fragment loginFragment = new LoginFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container,loginFragment,LoginFragment.TAG)
+                    .commit();
+        }
+
         // try to sign in with current user
         authViewModel.getUser();
-
-        TextWatcher invalidCredentialsWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // set login button to disabled if there is no text in either email or password
-                // or enabled if there is text in both
-                loginButton.setEnabled(!mEmail.getText().toString().isEmpty() && !mPassword.getText().toString().isEmpty());
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                // clear errors
-                emailTextInputLayout.setError(null);
-                passwordTextInputLayout.setError(null);
-            }
-        };
-
-        mEmail = findViewById(R.id.login_email);
-        emailTextInputLayout = findViewById(R.id.email_text_input_layout);
-        mEmail.addTextChangedListener(invalidCredentialsWatcher);
-
-        mPassword = findViewById(R.id.login_password);
-        passwordTextInputLayout = findViewById(R.id.password_text_input_layout);
-        mPassword.addTextChangedListener(invalidCredentialsWatcher);
-
-        loginButton = findViewById(R.id.login_button);
     }
 
-    private void signUserIn(User user) {
+    private void signUserIn() {
         Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(mainActivityIntent);
         finish();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            if (resultCode == RESULT_OK) {
-                signUserIn((User) data.getSerializableExtra("authenticated_user"));
-            } else {
-                Log.d(TAG, "Sign in cancelled");
-            }
-        }
-    }
-
-    public void resetPassword(View view) {
-        startActivity(new Intent(getApplicationContext(), ResetActivity.class));
-    }
-
-    public void login(final View view) {
-        // clear previous errors
-        emailTextInputLayout.setError(null);
-        passwordTextInputLayout.setError(null);
-
-        final String email = mEmail.getText().toString();
-        final String password = mPassword.getText().toString();
-
-        authViewModel.signInWithEmailAndPassword(email,password);
-    }
-
-    public void register(View view) {
-        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
     }
 }
