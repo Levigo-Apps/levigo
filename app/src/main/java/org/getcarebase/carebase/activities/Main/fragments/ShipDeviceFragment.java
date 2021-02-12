558b3bc0d1dadbc58243a17d6d7be0f4f469e83b
hackerrank <sahithb00@gmail.com>
2021-02-12 00:23:39 -0800

    Created and added navigation to device shipment view
    From the equipment details view, menu item click takes user to ship
    device view, which has the name, udi, and quantity of the device
    autofilled. No intentional design or saving feature yet.

--- app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
@@ -5,31 +5,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 import org.getcarebase.carebase.R;
+import org.getcarebase.carebase.viewmodels.DeviceViewModel;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 public class ShipDeviceFragment extends Fragment {
     private Activity parent;
     TextView deviceName;
+    TextView deviceUdi;
+    EditText deviceQty;
+    EditText deviceDest;
+
+    private View rootView;
+    private DeviceViewModel deviceViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        rootView = inflater.inflate(R.layout.ship_device, container, false);
         parent = getActivity();
-        final View rootView = inflater.inflate(R.layout.ship_device, container, false);
         MaterialToolbar topToolBar = rootView.findViewById(R.id.topAppBar);
+        deviceName = rootView.findViewById(R.id.device_name);
+        deviceUdi = rootView.findViewById(R.id.device_udi);
+        deviceQty = rootView.findViewById(R.id.device_quantity);
+        deviceDest = rootView.findViewById(R.id.device_dest);
+
+        // TODO: setup object to save destination input through device view model
+        deviceViewModel = new ViewModelProvider(this).get(DeviceViewModel.class);
+        handleArguments();
 
         topToolBar.setNavigationOnClickListener(view -> {
-            if (parent != null)
-                parent.onBackPressed();
+            if (requireActivity().getSupportFragmentManager().getBackStackEntryCount() > 0) {
+                requireActivity().getSupportFragmentManager().popBackStack();
+            } else {
+                if (parent != null)
+                    parent.onBackPressed();
+            }
         });
 
         return rootView;
+    }
+
+    public void handleArguments() {
+        if (getArguments() != null) {
+            String name, barcode, qty;
+            if ((name = getArguments().getString("name")) != null) {
+                deviceName.setText(name);
+            }
+            if ((barcode = getArguments().getString("barcode")) != null) {
+                deviceUdi.setText(barcode);
+            }
+            if ((qty = getArguments().getString("qty")) != null) {
+                deviceQty.setText(qty);
+            }
+        }
     }
 }
