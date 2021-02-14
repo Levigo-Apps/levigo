519cfb6ed22a782db9ccecdc35b154dd1d2c9fa5
hackerrank <sahithb00@gmail.com>
2021-02-13 22:23:48 -0800

    Made ship device view look more like intended
    Quantity and Destination inputs are now Material Text Layouts, and Save
    button is constrained to bottom of the view.

--- app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
@@ -5,25 +5,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.getcarebase.carebase.R;
 import org.getcarebase.carebase.viewmodels.DeviceViewModel;
 
+import java.util.Collections;
+
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 public class ShipDeviceFragment extends Fragment {
     private Activity parent;
     TextView deviceName;
     TextView deviceUdi;
-    EditText deviceQty;
-    EditText deviceDest;
+    TextInputLayout deviceQty;
+    TextInputLayout deviceDest;
 
     private View rootView;
     private DeviceViewModel deviceViewModel;
@@ -38,7 +41,11 @@ public class ShipDeviceFragment extends Fragment {
         deviceName = rootView.findViewById(R.id.device_name);
         deviceUdi = rootView.findViewById(R.id.device_udi);
         deviceQty = rootView.findViewById(R.id.device_quantity);
+
         deviceDest = rootView.findViewById(R.id.device_dest);
+        AutoCompleteTextView destOptions = (AutoCompleteTextView) deviceDest.getEditText();
+        // TODO: change hardcoding of list options for device destination
+        destOptions.setAdapter(new ArrayAdapter(requireContext(), R.layout.dropdown_menu_popup_item, Collections.singletonList("Muhimbili")));
 
         // TODO: setup object to save destination input through device view model
         deviceViewModel = new ViewModelProvider(this).get(DeviceViewModel.class);
@@ -57,6 +64,7 @@ public class ShipDeviceFragment extends Fragment {
     }
 
     public void handleArguments() {
+        // TODO: Autocomplete quantity
         if (getArguments() != null) {
             String name, barcode, qty;
             if ((name = getArguments().getString("name")) != null) {
@@ -64,9 +72,6 @@ public class ShipDeviceFragment extends Fragment {
             }
             if ((barcode = getArguments().getString("barcode")) != null) {
                 deviceUdi.setText(barcode);
-            }
-            if ((qty = getArguments().getString("qty")) != null) {
-                deviceQty.setText(qty);
             }
         }
     }
