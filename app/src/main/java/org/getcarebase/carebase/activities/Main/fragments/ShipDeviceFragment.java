230aadf2d32bff35bd35cd78fd101294b254fe76
hackerrank <sahithb00@gmail.com>
2021-02-18 14:35:32 -0800

    Adding shipping destination options in progress

--- app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
@@ -2,6 +2,7 @@ package org.getcarebase.carebase.activities.Main.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +15,18 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.getcarebase.carebase.R;
+import org.getcarebase.carebase.utils.Resource;
 import org.getcarebase.carebase.viewmodels.DeviceViewModel;
 
 import java.util.Collections;
+import java.util.List;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 public class ShipDeviceFragment extends Fragment {
+    public static final String TAG = ShipDeviceFragment.class.getName();
     private Activity parent;
     TextView deviceName;
     TextView deviceUdi;
@@ -44,11 +49,17 @@ public class ShipDeviceFragment extends Fragment {
 
         deviceDest = rootView.findViewById(R.id.device_dest);
         AutoCompleteTextView destOptions = (AutoCompleteTextView) deviceDest.getEditText();
-        // TODO: change hardcoding of list options for device destination
-        destOptions.setAdapter(new ArrayAdapter(requireContext(), R.layout.dropdown_menu_popup_item, Collections.singletonList("Muhimbili")));
+
+        deviceViewModel = new ViewModelProvider(this).get(DeviceViewModel.class);
+        deviceViewModel.getUserLiveData().observe(getViewLifecycleOwner(), userResource -> {
+            deviceViewModel.setupDeviceRepository();
+            LiveData<Resource<String[]>> siteOptions = deviceViewModel.getSitesLiveData();
+            Log.d(TAG, String.valueOf(siteOptions.getValue()));
+            destOptions.setAdapter(new ArrayAdapter(requireContext(), R.layout.dropdown_menu_popup_item,
+                    Collections.singletonList("Muhimbili")/*siteOptions.getValue().getData()*/));
+        });
 
         // TODO: setup object to save destination input through device view model
-        deviceViewModel = new ViewModelProvider(this).get(DeviceViewModel.class);
         handleArguments();
 
         topToolBar.setNavigationOnClickListener(view -> {
@@ -64,7 +75,6 @@ public class ShipDeviceFragment extends Fragment {
     }
 
     public void handleArguments() {
-        // TODO: Autocomplete quantity
         if (getArguments() != null) {
             String name, barcode, qty;
             if ((name = getArguments().getString("name")) != null) {
