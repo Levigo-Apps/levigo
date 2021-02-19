4b4328a05ca010b514cb9d168610aa806051ca6b
Saketh Dargula <sdargula@gmail.com>
2021-02-19 10:34:19 -0500

    reverted to old styles - removed analytics fragment

--- app/src/main/java/org/getcarebase/carebase/activities/Main/adapters/HomePagerAdapter.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Main/adapters/HomePagerAdapter.java
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import org.getcarebase.carebase.activities.Main.fragments.AnalyticsFragment;
 import org.getcarebase.carebase.activities.Main.fragments.InventoryFragment;
 import org.getcarebase.carebase.activities.Main.fragments.ProceduresFragment;
 
@@ -20,16 +19,13 @@ public class HomePagerAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position) {
             case 0: return new InventoryFragment();
-            // TODO change to ProcedureFragment
             case 1: return new ProceduresFragment();
-            // TODO change to Analytics Fragment
-            case 2: return new AnalyticsFragment();
             default: return new Fragment();
         }
     }
 
     @Override
     public int getItemCount() {
-        return 3;
+        return 2;
     }
 }
