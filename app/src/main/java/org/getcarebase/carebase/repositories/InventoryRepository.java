19873580da342b54677a3b2a19b4307b044d3496
Katherineweng <wengk@rpi.edu>
2021-02-22 15:50:57 -0500

    Revert "Changed inventory loading scheme."
    This reverts commit c739c3773b40bb73fd1cee63399c918e9db94890.

--- app/src/main/java/org/getcarebase/carebase/repositories/InventoryRepository.java
+++ app/src/main/java/org/getcarebase/carebase/repositories/InventoryRepository.java
@@ -1,15 +1,10 @@
 package org.getcarebase.carebase.repositories;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -32,83 +27,21 @@ import java.util.Objects;
  */
 public class InventoryRepository {
     private final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-
-    private List<String> deviceTypeList = new ArrayList<>();
     private final List<DeviceModel> deviceModelList = new ArrayList<>();
 
-//    /**
-//     * Populates the deviceModelList with all the models with all of it productions
-//     * @param user The currently signed in user
-//     * @return LiveData : Holds the resource (status) of the reference to the list
-//     */
-//    public LiveData<Resource<List<DeviceModel>>> getDeviceModelListForHospital(User user) {
-//        deviceModelList.clear();
-//        Resource<List<DeviceModel>> deviceModelListResource = new Resource<>(deviceModelList, new Request(null, Request.Status.LOADING));
-//        final MutableLiveData<Resource<List<DeviceModel>>> deviceModelListLiveData = new MutableLiveData<>(deviceModelListResource);
-//        String inventoryRefUrl = "networks/" + user.getNetworkId() + "/hospitals/"
-//                + user.getHospitalId() + "/departments/default_department/dis";
-//
-//        firestore.collection(inventoryRefUrl).get().addOnCompleteListener(task -> {
-//            if (task.isSuccessful()) {
-//                List<Task<QuerySnapshot>> tasks = new ArrayList<>();
-//                for (QueryDocumentSnapshot modelSnapshot : Objects.requireNonNull(task.getResult())) {
-//                    final DeviceModel deviceModel = new DeviceModel(modelSnapshot.getData());
-//
-//                    Task<QuerySnapshot> productionTask = modelSnapshot.getReference().collection("udis").get();
-//                    tasks.add(productionTask);
-//                    productionTask.addOnCompleteListener(completeProductionTask -> {
-//                        for (QueryDocumentSnapshot productionSnapshot : Objects.requireNonNull(completeProductionTask.getResult())) {
-//                            if (completeProductionTask.isSuccessful()) {
-//                                DeviceProduction deviceProduction = new DeviceProduction(productionSnapshot.getData());
-//                                deviceModel.addDeviceProduction(deviceProduction);
-//                            } else {
-//                                // error if getting all productions for a model fails
-//                                deviceModelListLiveData.setValue(new Resource<>(null, new Request(R.string.error_something_wrong, Request.Status.ERROR)));
-//                            }
-//                        }
-//                    });
-//                    deviceModelList.add(deviceModel);
-//                }
-//                Tasks.whenAllSuccess(tasks).addOnCompleteListener(task1 -> deviceModelListLiveData.setValue(new Resource<>(deviceModelList, new Request(null, Request.Status.SUCCESS))));
-//            } else {
-//                // error if getting all models fails
-//                deviceModelListLiveData.setValue(new Resource<>(null, new Request(R.string.error_something_wrong, Request.Status.ERROR)));
-//            }
-//        });
-//        return deviceModelListLiveData;
-//    }
-
-    // a function that gets all of the types in the inventory
-    public LiveData<Resource<List<String>>> getAllTypes(User user) {
-        deviceTypeList.clear();
-        Resource<List<String>> deviceTypeListResource = new Resource<>(deviceTypeList, new Request(null, Request.Status.LOADING));
-        final MutableLiveData<Resource<List<String>>> deviceTypeListLiveData = new MutableLiveData<>(deviceTypeListResource);
-
-        String inventoryRefUrl = "networks/" + user.getNetworkId() + "/hospitals";
-        firestore.collection(inventoryRefUrl).document(user.getHospitalId()).get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                DocumentSnapshot documentSnapshot = task.getResult();
-                deviceTypeList = (List<String>) documentSnapshot.get("device_types");
-                deviceTypeListLiveData.setValue(new Resource<>(deviceTypeList, new Request(null, Request.Status.SUCCESS)));
-            } else {
-                // error if getting Hospital inventory failed
-                deviceTypeListLiveData.setValue(new Resource<>(null, new Request(R.string.error_something_wrong, Request.Status.ERROR)));
-            }
-        });
-
-        return deviceTypeListLiveData;
-    }
-
-    // a function that gets the devices with the given type
-    public LiveData<Resource<List<DeviceModel>>> getDevicesWithType(User user, String selectedType) {
+    /**
+     * Populates the deviceModelList with all the models with all of it productions
+     * @param user The currently signed in user
+     * @return LiveData : Holds the resource (status) of the reference to the list
+     */
+    public LiveData<Resource<List<DeviceModel>>> getDeviceModelListForHospital(User user) {
         deviceModelList.clear();
         Resource<List<DeviceModel>> deviceModelListResource = new Resource<>(deviceModelList, new Request(null, Request.Status.LOADING));
         final MutableLiveData<Resource<List<DeviceModel>>> deviceModelListLiveData = new MutableLiveData<>(deviceModelListResource);
-
         String inventoryRefUrl = "networks/" + user.getNetworkId() + "/hospitals/"
                 + user.getHospitalId() + "/departments/default_department/dis";
 
-        firestore.collection(inventoryRefUrl).whereEqualTo("equipment_type", selectedType).get().addOnCompleteListener(task -> {
+        firestore.collection(inventoryRefUrl).get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 List<Task<QuerySnapshot>> tasks = new ArrayList<>();
                 for (QueryDocumentSnapshot modelSnapshot : Objects.requireNonNull(task.getResult())) {
@@ -129,15 +62,13 @@ public class InventoryRepository {
                     });
                     deviceModelList.add(deviceModel);
                 }
-                Tasks.whenAllSuccess(tasks).addOnCompleteListener(task1 -> deviceModelListLiveData.setValue(new Resource(deviceModelList, new Request(null, Request.Status.SUCCESS))));
+                Tasks.whenAllSuccess(tasks).addOnCompleteListener(task1 -> deviceModelListLiveData.setValue(new Resource<>(deviceModelList, new Request(null, Request.Status.SUCCESS))));
             } else {
                 // error if getting all models fails
-                deviceModelListLiveData.setValue(new Resource(null, new Request(R.string.error_something_wrong, Request.Status.ERROR)));
+                deviceModelListLiveData.setValue(new Resource<>(null, new Request(R.string.error_something_wrong, Request.Status.ERROR)));
             }
         });
-
         return deviceModelListLiveData;
     }
 
 }
-
--- app/src/main/java/org/getcarebase/carebase/viewmodels/InventoryViewModel.java
+++ app/src/main/java/org/getcarebase/carebase/viewmodels/InventoryViewModel.java
@@ -22,8 +22,8 @@ public class InventoryViewModel extends ViewModel {
     private final InventoryRepository inventoryRepository;
     private final FirebaseAuthRepository authRepository;
 
-//    private final SingleEventMediatorLiveData<List<DeviceModel>> inventoryLiveData = new SingleEventMediatorLiveData<>();
-//    private final LiveData<Resource<Map<String,List<DeviceModel>>>> categoricalInventoryLiveData = Transformations.map(inventoryLiveData.getLiveData(), this::sortInventoryByCategories);
+    private final SingleEventMediatorLiveData<List<DeviceModel>> inventoryLiveData = new SingleEventMediatorLiveData<>();
+    private final LiveData<Resource<Map<String,List<DeviceModel>>>> categoricalInventoryLiveData = Transformations.map(inventoryLiveData.getLiveData(), this::sortInventoryByCategories);
     private LiveData<Resource<User>> userLiveData;
 
     public InventoryViewModel() {
@@ -42,44 +42,35 @@ public class InventoryViewModel extends ViewModel {
         authRepository.signOut();
     }
 
-
-    public LiveData<Resource<List<String>>> getTypeListLiveData() {
-        return inventoryRepository.getAllTypes(Objects.requireNonNull(userLiveData.getValue()).getData());
+    /**
+     * Updates inventoryLiveData with the LiveData from the repository
+     */
+    public void loadInventory() {
+        inventoryLiveData.addSource(inventoryRepository.getDeviceModelListForHospital(Objects.requireNonNull(userLiveData.getValue()).getData()));
     }
 
-    public LiveData<Resource<List<DeviceModel>>> getDeviceModelListWithTypeLiveData(String type) {
-        return inventoryRepository.getDevicesWithType(Objects.requireNonNull(userLiveData.getValue()).getData(), type);
+    public LiveData<Resource<Map<String, List<DeviceModel>>>> getCategoricalInventoryLiveData() {
+        return categoricalInventoryLiveData;
     }
 
-//    /**
-//     * Updates inventoryLiveData with the LiveData from the repository
-//     */
-//    public void loadInventory() {
-//        inventoryLiveData.addSource(inventoryRepository.getDeviceModelListForHospital(Objects.requireNonNull(userLiveData.getValue()).getData()));
-//    }
-//
-//    public LiveData<Resource<Map<String, List<DeviceModel>>>> getCategoricalInventoryLiveData() {
-//        return categoricalInventoryLiveData;
-//    }
+    /**
+     * Sorts a list of devices from a repository call into a map of categories and devices
+     * @param listResource the result of the repository call
+     * @return the converted Resource
+     */
+    private Resource<Map<String,List<DeviceModel>>> sortInventoryByCategories(Resource<List<DeviceModel>> listResource) {
+        if (listResource.getRequest().getStatus() == Request.Status.ERROR
+                || listResource.getRequest().getStatus() == Request.Status.LOADING) {
+            return new Resource<>(null,listResource.getRequest());
+        }
 
-//    /**
-//     * Sorts a list of devices from a repository call into a map of categories and devices
-//     * @param listResource the result of the repository call
-//     * @return the converted Resource
-//     */
-//    private Resource<Map<String,List<DeviceModel>>> sortInventoryByCategories(Resource<List<DeviceModel>> listResource) {
-//        if (listResource.getRequest().getStatus() == Request.Status.ERROR
-//                || listResource.getRequest().getStatus() == Request.Status.LOADING) {
-//            return new Resource<>(null,listResource.getRequest());
-//        }
-//
-//        Map<String,List<DeviceModel>> categoricalInventoryMap = new HashMap<>();
-//        List<DeviceModel> deviceModelList = listResource.getData();
-//        for (DeviceModel deviceModel : deviceModelList) {
-//            List<DeviceModel> category = categoricalInventoryMap.getOrDefault(deviceModel.getEquipmentType(), new ArrayList<>());
-//            category.add(deviceModel);
-//            categoricalInventoryMap.putIfAbsent(deviceModel.getEquipmentType(), category);
-//        }
-//        return new Resource<>(categoricalInventoryMap,new Request(null, Request.Status.SUCCESS));
-//    }
+        Map<String,List<DeviceModel>> categoricalInventoryMap = new HashMap<>();
+        List<DeviceModel> deviceModelList = listResource.getData();
+        for (DeviceModel deviceModel : deviceModelList) {
+            List<DeviceModel> category = categoricalInventoryMap.getOrDefault(deviceModel.getEquipmentType(), new ArrayList<>());
+            category.add(deviceModel);
+            categoricalInventoryMap.putIfAbsent(deviceModel.getEquipmentType(), category);
+        }
+        return new Resource<>(categoricalInventoryMap,new Request(null, Request.Status.SUCCESS));
+    }
 }
