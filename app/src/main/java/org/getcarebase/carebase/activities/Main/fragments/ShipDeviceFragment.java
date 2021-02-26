63fe9993bb984a74bc637c820642b8bdd5cff37b
hackerrank <sahithb00@gmail.com>
2021-02-26 08:58:20 -0800

    Revert "Merge pull request #129 from Carebase-Solutions/save_shipment"
    This reverts commit 244d3d61e23f1309cf13dc433a8258761e12f105, reversing
    changes made to 1c1ae1e7558b9f95b894ab4d89f4664cc8fb08c2.

--- app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
@@ -8,25 +8,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.getcarebase.carebase.R;
-import org.getcarebase.carebase.models.Shipment;
-import org.getcarebase.carebase.utils.Request;
+import org.getcarebase.carebase.utils.Resource;
 import org.getcarebase.carebase.viewmodels.DeviceViewModel;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 public class ShipDeviceFragment extends Fragment {
@@ -36,10 +35,6 @@ public class ShipDeviceFragment extends Fragment {
     TextView deviceUdi;
     TextInputLayout deviceQty;
     TextInputLayout deviceDest;
-
-    Button saveButton;
-
-    String currentHospitalName;
 
     private View rootView;
     private DeviceViewModel deviceViewModel;
@@ -58,21 +53,16 @@ public class ShipDeviceFragment extends Fragment {
         deviceDest = rootView.findViewById(R.id.device_dest);
         AutoCompleteTextView destOptions = (AutoCompleteTextView) deviceDest.getEditText();
 
-        saveButton = rootView.findViewById(R.id.save_shipment);
-
         deviceViewModel = new ViewModelProvider(this).get(DeviceViewModel.class);
 
         final ArrayAdapter<String> sitesAdapter = new ArrayAdapter<>(rootView.getContext(), R.layout.dropdown_menu_popup_item, new ArrayList<>());
         deviceViewModel.getUserLiveData().observe(getViewLifecycleOwner(), userResource -> {
             deviceViewModel.setupDeviceRepository();
-            if (userResource.getRequest().getStatus() == org.getcarebase.carebase.utils.Request.Status.SUCCESS) {
-                currentHospitalName = userResource.getData().getHospitalName();
-                deviceViewModel.setHospitalRepository(userResource.getData().getHospitalId());
-            }
+            LiveData<Resource<String[]>> siteOptions = deviceViewModel.getSitesLiveData();
             deviceViewModel.getSitesLiveData().observe(getViewLifecycleOwner(), sitesResource -> {
                 if(sitesResource.getRequest().getStatus() == org.getcarebase.carebase.utils.Request.Status.SUCCESS) {
                     sitesAdapter.clear();
-                    sitesAdapter.addAll(sitesResource.getData().values());
+                    sitesAdapter.addAll(Arrays.asList(sitesResource.getData()));
                 } else {
                     Log.d(TAG,"Unable to fetch sites");
                     Snackbar.make(rootView, R.string.error_something_wrong, Snackbar.LENGTH_LONG).show();
@@ -81,21 +71,8 @@ public class ShipDeviceFragment extends Fragment {
         });
         destOptions.setAdapter(sitesAdapter);
 
+        // TODO: setup object to save destination input through device view model
         handleArguments();
-
-        deviceViewModel.getSaveShipmentRequestLiveData().observe(getViewLifecycleOwner(), request -> {
-            if (request.getStatus() == Request.Status.SUCCESS) {
-                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-                View nextView = requireActivity().findViewById(R.id.activity_main);
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.remove(this).commit();
-                Snackbar.make(nextView, "Shipment saved to inventory", Snackbar.LENGTH_LONG).show();
-            } else {
-                Log.d(TAG,"error while saving shipment");
-                Snackbar.make(rootView, R.string.error_something_wrong, Snackbar.LENGTH_LONG).show();
-            }
-        });
-        saveButton.setOnClickListener(v -> saveData());
 
         topToolBar.setNavigationOnClickListener(view -> {
             if (requireActivity().getSupportFragmentManager().getBackStackEntryCount() > 0) {
@@ -122,41 +99,5 @@ public class ShipDeviceFragment extends Fragment {
                 deviceQty.getEditText().setText(qty);
             }
         }
-    }
-
-    private void saveData() {
-        // TODO: Check if inputs are complete and valid
-        Shipment shipment = new Shipment();
-        shipment.setUdi((String) deviceUdi.getText());
-        String di;
-        shipment.setDi((di = getArguments().getString("di")) != null ? di : "");
-        Integer shippedQuantity = Integer.parseInt(deviceQty.getEditText().getText().toString());
-        shipment.setShippedQuantity(shippedQuantity);
-
-        // TODO: Reduce quantities of device model and device production
-//        deviceViewModel.getDeviceInFirebaseLiveData().observe(getViewLifecycleOwner(), deviceModelResource -> {
-//            DeviceModel deviceModel = deviceModelResource.getData();
-//            deviceModel.setQuantity(deviceModel.getQuantity() - shippedQuantity);
-//            deviceViewModel.saveDevice(deviceModel);
-//        });
-        
-        deviceViewModel.getSitesLiveData().observe(getViewLifecycleOwner(), sitesResource -> {
-            Map<String, String> sitesMap = sitesResource.getData();
-            Boolean destSet = false, sourceSet = false;
-            for (String id : sitesMap.keySet()) {
-                if (Objects.requireNonNull(deviceDest.getEditText()).getText().toString().contentEquals(sitesMap.get(id))) {
-                    shipment.setDestinationHospitalId(id);
-                    deviceViewModel.setHospitalRepository(id);
-                    destSet = true;
-                }
-                if (currentHospitalName.contentEquals(sitesMap.get(id))) {
-                    shipment.setSourceHospitalId(id);
-                    sourceSet = true;
-                }
-                if (destSet && sourceSet) break;
-            }
-
-            deviceViewModel.saveShipment(shipment);
-        });
     }
 }
