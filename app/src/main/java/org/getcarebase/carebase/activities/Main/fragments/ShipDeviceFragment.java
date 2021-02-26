67ea527076b3fc218ca884f1847828efa809a41a
hackerrank <sahithb00@gmail.com>
2021-02-26 08:51:32 -0800

    Changed destination hospital field name

--- app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Main/fragments/ShipDeviceFragment.java
@@ -16,7 +16,6 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.getcarebase.carebase.R;
-import org.getcarebase.carebase.models.DeviceModel;
 import org.getcarebase.carebase.models.Shipment;
 import org.getcarebase.carebase.utils.Request;
 import org.getcarebase.carebase.viewmodels.DeviceViewModel;
@@ -146,7 +145,7 @@ public class ShipDeviceFragment extends Fragment {
             Boolean destSet = false, sourceSet = false;
             for (String id : sitesMap.keySet()) {
                 if (Objects.requireNonNull(deviceDest.getEditText()).getText().toString().contentEquals(sitesMap.get(id))) {
-                    shipment.setDestHospital(id);
+                    shipment.setDestinationHospitalId(id);
                     deviceViewModel.setHospitalRepository(id);
                     destSet = true;
                 }
