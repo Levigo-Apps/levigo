74fa4b1772a8adc8e047991a0378ddaa994b1405
Saketh Dargula <sdargula@gmail.com>
2021-04-20 11:45:11 -0400

    implemented new scanner for procedures

--- app/src/main/java/org/getcarebase/carebase/activities/Main/CarebaseScanningActivity.java
+++ app/src/main/java/org/getcarebase/carebase/activities/Main/CarebaseScanningActivity.java
@@ -1,6 +1,9 @@
 package org.getcarebase.carebase.activities.Main;
 ;
 import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 import com.carebase.carebasescanner.ScanningActivity;
 
@@ -9,6 +12,14 @@ import org.getcarebase.carebase.activities.Main.fragments.AutoPopulatedBarcodeRe
 public class CarebaseScanningActivity extends ScanningActivity {
     private static final String TAG = CarebaseScanningActivity.class.getSimpleName();
     public static final String ARG_UDI_RESULT = "arg_udi_result";
+    private int resultCode;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.resultCode = getIntent().getIntExtra("result_code",0);
+
+    }
 
     // TODO build custom bottom sheet
     @Override
@@ -23,7 +34,7 @@ public class CarebaseScanningActivity extends ScanningActivity {
         // returns scanned result
         Intent data = new Intent();
         data.putExtra(CarebaseScanningActivity.ARG_UDI_RESULT,udi);
-        setResult(MainActivity.RESULT_SCANNED,data);
+        setResult(resultCode,data);
         finish();
     }
 }
